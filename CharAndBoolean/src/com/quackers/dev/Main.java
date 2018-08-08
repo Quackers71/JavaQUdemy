8b282b714b860ec1e1de21c2be1b5f2cdd355546
Quacks <24840026+Quackers71@users.noreply.github.com>
2018-08-08 23:21:14 +0100

    Char and Boolean

---
+++ CharAndBoolean/src/com/quackers/dev/Main.java
@@ -0,0 +1,18 @@
+package com.quackers.dev;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char theChar = '\u00A9';
+        char regSymbol = '\u00AE';
+
+        System.out.println(theChar);
+        System.out.println(regSymbol);
+
+        boolean myBoolean = false;
+        boolean isMale = true;
+
+
+    }
+}
