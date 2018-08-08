f2eeaba6c23b75272293f2573edc6793cb960058
quackers71 <24840026+Quackers71@users.noreply.github.com>
2018-08-08 16:46:16 +0100

    int datatype

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -2,5 +2,17 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        int mySubTotal = 1000;
+
+        int myNewTotal = mySubTotal - myTotal;
+
+        System.out.println(myNewTotal);
     }
 }
