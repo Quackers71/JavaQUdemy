46e16e487c34e5bc2375dbc14782fa86e87517cc
Quacks <24840026+Quackers71@users.noreply.github.com>
2018-08-09 23:21:04 +0100

    Keywords and Expressions

---
+++ StatementsWhiteSpaceAndIndenting/src/com/quackers/dev/Main.java
@@ -0,0 +1,28 @@
+package com.quackers.dev;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int myVariable = 50; //statement & assignment expression
+        myVariable++; //statement
+        myVariable--; //statement
+        System.out.println("This is a test"); //statement / method call
+
+        System.out.println("This is " +
+                "another " +
+                "and more");
+
+        int anotherVariable = 50;myVariable--;System.out.println("And another one!");
+
+        /**
+         *  This is indentation
+         *
+         *      int myVariable2 = 60;
+         *      if (myVariable2 >= 50) {
+         *          System.out.println("This is an indentation test");
+         *      }
+         */
+
+    }
+}
