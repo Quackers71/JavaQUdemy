dbd3b39f4c922bcbb90a5b06856a5a04d17fcd27
Quacks <24840026+Quackers71@users.noreply.github.com>
2018-08-11 19:40:17 +0100

    My Attempt

--- Methods/src/com/quackers/dev/Main.java
+++ Methods/src/com/quackers/dev/Main.java
@@ -4,30 +4,84 @@ public class Main {
 
     public static void main(String[] args) {
 
+
         boolean gameOver = true;
-        int score = 800;
+        int score = 900;
         int levelCompleted = 5;
         int bonus = 100;
+        String player = "Dave";
 
-        calculateScore(gameOver, score,levelCompleted,bonus);
+        int highScore = calculateScore(gameOver, score,levelCompleted,bonus);
+        int Position = displayHighScorePosition(highScore);
+        System.out.println( player + "'s final score was " + highScore + " and managed to get into position " + Position);
 
-        score = 10000;
-        levelCompleted = 8;
+        score = 300;
+        levelCompleted = 5;
+        bonus = 100;
+        player = "Trev";
+
+        highScore = calculateScore(gameOver, score,levelCompleted,bonus);
+        Position = displayHighScorePosition(highScore);
+        System.out.println( player + "'s final score was " + highScore + " and managed to get into position " + Position);
+
+        score = 100;
+        levelCompleted = 2;
+        bonus = 100;
+        player = "Bob";
+
+        highScore = calculateScore(gameOver, score,levelCompleted,bonus);
+        Position = displayHighScorePosition(highScore);
+        System.out.println( player + "'s  final score was " + highScore + " and managed to get into position " + Position);
+
+        score = 50;
+        levelCompleted = 0;
         bonus = 200;
+        player = "John";
 
-        calculateScore(gameOver, score,levelCompleted,bonus);
+        highScore = calculateScore(gameOver, score,levelCompleted,bonus);
+        highScore -= 100;
+        Position = displayHighScorePosition(highScore);
+        System.out.println( player + "'s final score was " + highScore + " and managed to get into position " + Position);
     }
 
     public static int calculateScore (boolean gameOver, int score, int levelCompleted, int bonus) {
 
         if (gameOver) {
             int finalScore = score + (levelCompleted * bonus);
-            finalScore += 2000;
-            System.out.println("Your final score was " + finalScore);
+            finalScore += 100;
+
             return finalScore;
         }
         else {
             return -1;
         }
     }
+
+    public static int displayHighScorePosition (int highScore) {
+
+            if (highScore >= 1000 ) {
+                int scorePosition = 1;
+                return  scorePosition;
+            }
+            else if (highScore >= 500 && highScore < 1000) {
+                int scorePosition = 2;
+                return  scorePosition;
+            }
+            else if (highScore >= 100 && highScore < 500) {
+                int scorePosition = 3;
+                return  scorePosition;
+            }
+            else {
+                int scorePosition = 4;
+                return  scorePosition;
+            }
+
+    }
+
+    public static void calculateHighScorePosition (String player, int Score) {
+
+        // lovin this shit, but still learning
+    }
+
+
 }
